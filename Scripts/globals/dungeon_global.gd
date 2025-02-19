8ab96af726376b48e328001aa5a71d0a44643abe
minmagdev <levi.drieling@gmail.com>
2025-02-19 16:54:36 +0100

    Good progress in Dungeongeneration (Backend) - no doors

--- Scripts/globals/dungeon_global.gd
+++ Scripts/globals/dungeon_global.gd
@@ -3,19 +3,71 @@ extends Node
 var up_door_scn :=   preload("res://scenes/up_door.tscn")
 var side_door_scn := preload("res://scenes/up_door.tscn")
 
+var exit_room_exists := false
+
 enum  Difficulty {
 	EASY,
 	NORMAL,
 	HARD
 }
 
+enum  RoomTypes {
+	ENTRANCE,
+	ENEMY,
+	TREASURE,
+	EXIT
+}
+
+var room_distriubution := [
+	RoomTypes.ENEMY,
+	RoomTypes.ENEMY,
+	RoomTypes.TREASURE
+]
+
+var extra_doors_in_room := [false, false, false]
+
 func generate_dungeon(dungeon_size: int) -> Array[DungeonRoom]:
+	exit_room_exists = false
 	var dungeon_layout := generation_worker(dungeon_size, 1, [])
 	return dungeon_layout
 
 func generation_worker(depth: int, rooms_in_layer:int, acc: Array[DungeonRoom]) -> Array[DungeonRoom]:
+	if depth == 0:
+		return acc
+	for i in range(0,rooms_in_layer):
+		var new_room = generate_room(depth, i)
+		acc.append(new_room)
+	#TODO: Rekursion des Room-Workers
 	return acc
 
+func generate_room(layer: int, room_number: int) -> DungeonRoom:
+	var is_exit_room: bool = (randi_range(1, layer) == 1) and not exit_room_exists
+	var coordinate = Vector2(layer, room_number)
+	if is_exit_room: 
+		exit_room_exists = true
+		return ExitRoom.new([], coordinate)
+	var room_type = room_distriubution[randi_range(0, room_distriubution.size() - 1)]
+	var new_room : DungeonRoom
+	
+	var doors = generate_doors()
+	match room_type:
+		RoomTypes.ENEMY:
+			new_room = EnemyRoom.new(doors, coordinate)
+			
+		RoomTypes.TREASURE:
+			new_room = TreasureRoom.new(doors, coordinate)
+			pass
+	
+	return new_room
+
+func generate_doors() -> Array[Door]:
+	#TODO: Door Generator
+	return []
+
+
+#---------------------------------------------------------
+#CLASSES
+#---------------------------------------------------------
 class Door:
 	var door_position: Dictionary
 	var leads_to_room: DungeonRoom
@@ -54,10 +106,37 @@ class EnemyRoom extends DungeonRoom:
 	var room_difficulty: Difficulty
 	var enemys: Array[Node2D]
 	
-	func _init(_doors: Array[Door], _coordinate: Vector2, _difficulty: Difficulty) -> void:
-		room_difficulty = _difficulty
+	func _init(_doors: Array[Door], _coordinate: Vector2) -> void:
+		room_difficulty = generate_difficulty()
 		enemys = generate_monsters()
 		super(_doors, _coordinate)
+		
+	#Generate the room difficulty for Enemy Rooms
+	func generate_difficulty() -> Difficulty:
+		var room_diffculty : Difficulty
+		match randi_range(0, 4):
+			0:     room_diffculty = Difficulty.EASY
+			1,2,3: room_diffculty = Difficulty.NORMAL
+			4:     room_diffculty = Difficulty.HARD
+		return room_diffculty
+		
 	
 	func generate_monsters():
+		#TODO Generate Monsters in Enemey Room
 		return []
+
+class TreasureRoom extends DungeonRoom:
+	var treasure_rarity: int
+	
+	func _init(_doors: Array[Door], _coordinate: Vector2) -> void:
+		generate_treasure()
+		super(_doors, _coordinate)
+	
+	func generate_treasure():
+		#TODO: Whole Treasure System Lol
+		treasure_rarity = 1
+
+class ExitRoom extends DungeonRoom:
+	func _init(_doors: Array[Door], _coordinate: Vector2):
+		doors = []
+		coordinate = _coordinate

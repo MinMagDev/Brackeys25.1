d50523f3be425513342720716c0fd94243bc04a6
minmagdev <levi.drieling@gmail.com>
2025-02-19 14:59:00 +0100

    Started Dungeon Generation, far from ready

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,3 +3,6 @@ extends Node2D
 func _on_player_shoot_bullet(start_position: Vector2, direction: Vector2) -> void:
 	var projectile = Projectile.new_projectile(start_position, direction)
 	$Projectiles.add_child(projectile)
+
+func generate_dungeon(max_depth: int):
+	pass

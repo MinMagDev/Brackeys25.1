d237f6aced0e49e64dbaca02efa75c94d0a6b559
minmagdev <levi.drieling@gmail.com>
2025-02-19 20:11:39 +0100

    Nearly finished Dungeon generation, a few Bug fixes needed

--- Scripts/globals/dungeon_global.gd
+++ Scripts/globals/dungeon_global.gd
@@ -1,9 +1,11 @@
 extends Node
 
-var up_door_scn :=   preload("res://scenes/up_door.tscn")
+var up_door_scn   := preload("res://scenes/up_door.tscn")
 var side_door_scn := preload("res://scenes/up_door.tscn")
 
-var exit_room_exists := false
+var exit_room_exists : bool = false
+
+var rooms_in_next_layer : int = 1
 
 enum  Difficulty {
 	EASY,
@@ -34,13 +36,20 @@ func generate_dungeon(dungeon_size: int) -> Array[DungeonRoom]:
 func generation_worker(depth: int, rooms_in_layer:int, acc: Array[DungeonRoom]) -> Array[DungeonRoom]:
 	if depth == 0:
 		return acc
+	
+	print(" New Layer: ", depth)
+	rooms_in_next_layer = 1
+	#Generate Rooms in a Layer
 	for i in range(0,rooms_in_layer):
-		var new_room = generate_room(depth, i)
+		print("Yay new room")
+		var left_door :bool = false
+		if i != 0:
+			left_door = acc.back().has_right_door
+		var new_room = generate_room(depth, i + 1, rooms_in_layer, left_door)
 		acc.append(new_room)
-	#TODO: Rekursion des Room-Workers
-	return acc
+	return generation_worker(depth - 1, rooms_in_next_layer, acc)
 
-func generate_room(layer: int, room_number: int) -> DungeonRoom:
+func generate_room(layer: int, room_number: int, rooms_in_layer: int, left_door: bool) -> DungeonRoom:
 	var is_exit_room: bool = (randi_range(1, layer) == 1) and not exit_room_exists
 	var coordinate = Vector2(layer, room_number)
 	if is_exit_room: 
@@ -49,7 +58,8 @@ func generate_room(layer: int, room_number: int) -> DungeonRoom:
 	var room_type = room_distriubution[randi_range(0, room_distriubution.size() - 1)]
 	var new_room : DungeonRoom
 	
-	var doors = generate_doors()
+	var right_door_possible : bool = rooms_in_layer != room_number
+	var doors : Array[Door] = generate_doors(layer, room_number, left_door, right_door_possible)
 	match room_type:
 		RoomTypes.ENEMY:
 			new_room = EnemyRoom.new(doors, coordinate)
@@ -60,27 +70,86 @@ func generate_room(layer: int, room_number: int) -> DungeonRoom:
 	
 	return new_room
 
-func generate_doors() -> Array[Door]:
-	#TODO: Door Generator
-	return []
+func generate_doors(layer: int,
+					room_in_layer: int, 
+					left_door: bool , 
+					right_door_possible: bool
+					) -> Array[Door]:
+	var doors : Array[Door] = []
+	if left_door:
+		var new_door = Door.new(
+			Vector2(layer, room_in_layer - 1),
+			side_door_scn.instantiate()
+			)
+		new_door.set_position(Vector2(layer, room_in_layer), 0)
+		doors.append(new_door)
+	
+	#Second Up Door?
+	if randi_range(0,3) == 0:
+		for i in range(1):
+			var new_door = Door.new(
+				Vector2(layer - 1, rooms_in_next_layer),
+				side_door_scn.instantiate()
+			)
+			new_door.set_position(Vector2(layer, room_in_layer), 1 + i*2)
+			doors.append(new_door)
+			rooms_in_next_layer += 1
+	else: #The only one door
+		var new_door = Door.new(
+			Vector2(layer - 1, rooms_in_next_layer),
+			side_door_scn.instantiate()
+		)
+		new_door.set_position(Vector2(layer, room_in_layer), 2)
+		doors.append(new_door)
+		rooms_in_next_layer += 1
+	
+	# Possiblity to create a doorway to the adjacent right room
+	if right_door_possible and randi_range(0,4) == 0:
+		var new_door = Door.new(
+			Vector2(layer, room_in_layer + 1),
+			side_door_scn.instantiate()
+		)
+		new_door.set_position(Vector2(layer, room_in_layer), 4)
+		doors.append(new_door)
+	return doors
 
+
+func print_dungeon(dungeon: Array[DungeonRoom]) -> void:
+	var count = 0
+	for room in dungeon:
+		print("Room Number: " + str(count))
+		print("Is in layer: " + str(room.coordinate.x) + " at position: "+ str(room.coordinate.y))
+		print("It is from the type: " + str(room.get_type()))
+		print("Amount of doors: " + str(room.doors.size()))
+		print("The rooms lead to: ")
+		var door_count = 0
+		for door in room.doors:
+			print("    Door number: " + str(door_count))
+			print("    Position in room: " + str(door.door_position["in_room_position"]))
+			print("    Leads to: " + str(door.leads_to_room))
+			door_count += 1
 
 #---------------------------------------------------------
 #CLASSES
 #---------------------------------------------------------
 class Door:
 	var door_position: Dictionary
-	var leads_to_room: DungeonRoom
+	var leads_to_room: Vector2
 	var door_node: Node2D
 	
-	func set_position(room: DungeonRoom, in_room_position: int):
+	func set_position(room: Vector2, in_room_position: int):
 		door_position = {
 			"room" : room,
+			
+			#INFO: 
+			# 0 = Left
+			# 1,2,3 = Up (either 1 and 2) or only 3 in one room
+			# 4 = right
 			"in_room_position": in_room_position
+	
 		}
 	
-	func _init(_door_position: Dictionary, _leads_to_room: DungeonRoom, _door_node) -> void:
-		door_position =_door_position
+	func _init(_leads_to_room: Vector2, _door_node) -> void:
 		leads_to_room = _leads_to_room
 		door_node = _door_node
 		door_node.leads_to_room = leads_to_room
@@ -88,10 +157,14 @@ class Door:
 class DungeonRoom:
 	var doors : Array[Door]
 	var coordinate : Vector2
+	var has_right_door: bool
 	
 	func _init(_doors: Array[Door], _coordinate: Vector2) -> void:
 		doors = _doors
 		coordinate = _coordinate
+	
+	func get_type() -> String:
+		return "base"
 
 #------------------------------------------------------------------------
 #Different room Types
@@ -101,10 +174,14 @@ class EntranceRoom extends DungeonRoom:
 	func _init(_doors: Array[Door], _coordinate: Vector2, _artifact: int) -> void:
 		artifact = _artifact
 		super(_doors, _coordinate)
+	
+	func get_type() -> String:
+		return "EntranceRoom"
 
 class EnemyRoom extends DungeonRoom:
 	var room_difficulty: Difficulty
 	var enemys: Array[Node2D]
+	
 	
 	func _init(_doors: Array[Door], _coordinate: Vector2) -> void:
 		room_difficulty = generate_difficulty()
@@ -121,9 +198,12 @@ class EnemyRoom extends DungeonRoom:
 		return room_diffculty
 		
 	
-	func generate_monsters():
+	func generate_monsters() -> Array[Node2D]:
 		#TODO Generate Monsters in Enemey Room
 		return []
+	
+	func get_type() -> String:
+		return "EnemyRoom"
 
 class TreasureRoom extends DungeonRoom:
 	var treasure_rarity: int
@@ -135,8 +215,15 @@ class TreasureRoom extends DungeonRoom:
 	func generate_treasure():
 		#TODO: Whole Treasure System Lol
 		treasure_rarity = 1
+	
+	func get_type() -> String:
+		return "TreasureRoom"
+
 
 class ExitRoom extends DungeonRoom:
 	func _init(_doors: Array[Door], _coordinate: Vector2):
 		doors = []
 		coordinate = _coordinate
+		
+	func get_type() -> String:
+		return "ExitRoom"
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,3 +3,6 @@ extends Node2D
 func _on_player_shoot_bullet(start_position: Vector2, direction: Vector2) -> void:
 	var projectile = Projectile.new_projectile(start_position, direction)
 	$Projectiles.add_child(projectile)
+
+func _ready() -> void:
+	DungeonGlobal.print_dungeon(DungeonGlobal.generate_dungeon(5))
